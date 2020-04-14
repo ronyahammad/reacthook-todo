1f8f31526726fb53ac4d3678051a8d813b78b0db
Ali Ahammad <45920358+ronyahammad@users.noreply.github.com>
2020-04-14 23:27:11 +0100

    Updates

---
+++ precache-manifest.6d6af1e0a6aace4dc283278b69a5130d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c6c4a0aa24ca095ab938b83493d7d3",
+    "url": "/reacthook-todo/index.html"
+  },
+  {
+    "revision": "382f7b65cac1f9c815b2",
+    "url": "/reacthook-todo/static/css/main.a6d85274.chunk.css"
+  },
+  {
+    "revision": "b0c426fc7e8708299f0e",
+    "url": "/reacthook-todo/static/js/2.05005a45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthook-todo/static/js/2.05005a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382f7b65cac1f9c815b2",
+    "url": "/reacthook-todo/static/js/main.3e3b9680.chunk.js"
+  },
+  {
+    "revision": "98878b552ae22928f130",
+    "url": "/reacthook-todo/static/js/runtime-main.aff070b1.js"
+  }
+]);
